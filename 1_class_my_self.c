eb8deb326bc73d683f7bfe2e086b910b4dcb4524
chandaniCoder <chandanikumari140808@gmail.com>
2026-06-07 12:30:38 +0530

    Create 1_class_my_self.c

---
+++ 1_class_my_self.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf("Hello, My name is chandani.\n");
+    printf("I live in Delhi.\n");
+    printf("My school name is S.K.V. Rohini Sec-20.\n");
+    printf("My friend name is Varsha.\n");
+    printf("My sister name is Aarya.\n");
+    printf("My favorite subject is History.\n");
+    printf("I Respect My Parents And teachers.\n");
+    
+}
