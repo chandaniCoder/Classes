39454c6de6a9b70bc63b89bc00bb019be5f50ecb
chandaniCoder <chandanikumari140808@gmail.com>
2026-06-11 12:39:31 +0530

    Create 3_class_variable.c

---
+++ 3_class_variable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int year = 2026;
+   int month = 6;
+   int date = 13;
+   printf("today is %ith june %i\n",date,year);
+  printf("today is %ith june %i\n",date,year);
+  printf("today is %ith june %i\n",date,year);
+  printf("today is %ith june %i\n",date,year);
+  printf("today is %ith june %i\n",date,year);
+  printf("today is %ith june %i\n",date,year);
+  printf("today is %ith june %i\n",date,year);
+  printf("today is %ith june %i\n",date,year);
+  printf("today is %ith june %i\n",date,year);
+  printf("today is %ith june %i\n",date,year);
+  printf("today is %ith june %i\n",date,year);
+  }
