61912d03811ec9cb30e1e771155d7b1f2b4304c4
chandaniCoder <chandanikumari140808@gmail.com>
2026-06-11 12:06:35 +0530

    Create third_class_third_programme.c

---
+++ third_class_third_programm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int year = 2026;
+   int month = 6;
+   int date = 11;
+   printf("%i/0%i/%i\n",year,month,date);
+   printf("%i/0%i/%i\n",date,month, year);
+   
+}
+    
