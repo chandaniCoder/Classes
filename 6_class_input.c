6a02abf3a056a903def333dbee61c0c1b6286734
chandaniCoder <chandanikumari140808@gmail.com>
2026-06-20 12:18:54 +0530

    Create 6_class_input.c

---
+++ 6_class_input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // declaring an Variable named num with no value
+    int num ;
+    // printing input line
+    printf("enter the number: " );
+    // taking input for num variable
+    scanf("%d",& num);
+    // printing valu for not num variable
+    printf("number is: %d\n",num);
+   
+
+}
